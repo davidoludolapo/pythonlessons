6f0d72f72e8c3b0fe393397081841497ac666473
unknown <davidoluwadolapo@gmail.com>
2023-10-25 05:21:27 +0100

    working with for loops

--- practice.py
+++ practice.py
@@ -18,114 +18,177 @@
 # print("Mars is also known as", planets[3])
 
 #Determinng the length of a list
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-number_of_planets = len(planets)
-print("There are", number_of_planets, "planets in the solar system.")
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# number_of_planets = len(planets)
+# print("There are", number_of_planets, "planets in the solar system.")
 
-#Add values to lists
+# #Add values to lists
 
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-planets.append("Pluto")
-number_of_planets = len(planets)
-print("There are actually", number_of_planets, "planets in the solar system.")
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# planets.append("Pluto")
+# number_of_planets = len(planets)
+# print("There are actually", number_of_planets, "planets in the solar system.")
 
-#Remove value from lists
+# #Remove value from lists
 
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
-planets.pop()  # Goodbye, Pluto
-number_of_planets = len(planets)
-print("No, there are definitely", number_of_planets, "planets in the solar system.")
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+# planets.pop()  # Goodbye, Pluto
+# number_of_planets = len(planets)
+# print("No, there are definitely", number_of_planets, "planets in the solar system.")
 
-#Use negative indexes
+# #Use negative indexes
 
-print("The first planet is", planets[0])
+# print("The first planet is", planets[0])
 
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-print("The last planet is", planets[-1])
-print("The penultimate planet is", planets[-2])
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# print("The last planet is", planets[-1])
+# print("The penultimate planet is", planets[-2])
 
-#Find a value in a list
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-jupiter_index = planets.index("Jupiter")
-print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+# #Find a value in a list
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# jupiter_index = planets.index("Jupiter")
+# print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+# #Exercise
+
+# planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+# print(planets)
+
+# planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+# print(f"There are", len(planets), "planets")
+
+# planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+# planets.append('Pluto')
+# print("Actually, there are", len(planets), "planets")
+
+# print(planets[-1], "is the last planet")
+
+
+
+# #Use min() and max() with lists
+
+# gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+# bus_weight = 12650 # in Newtons, on Earth
+
+# print("On Earth, a double-decker bus weighs", bus_weight, "N")
+# print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "N")
+# print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "N")
+
+# #Slice Lists
+
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# planets_before_earth = planets[0:2]
+# print(planets_before_earth)
+
+# #To get all the planets after Earth, start at the third and go to the eightplanets_after_earth = 
+
+# planets_after_earth = planets[3:8]
+# print(planets_after_earth)
+
+# #Join Lists
+
+# amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+# galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+# regular_satellite_moons = amalthea_group + galilean_moons
+# print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+# #Sort Lists
+
+# amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+# galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+# regular_satellite_moons = amalthea_group + galilean_moons
+# regular_satellite_moons.sort()
+# print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+# #Reverse sort
+
+# amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+# galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+# regular_satellite_moons = amalthea_group + galilean_moons
+# regular_satellite_moons.sort(reverse=True)
+# print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
 
 #Exercise
 
-planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+# user_planets = input("Please enter the name of the planet (with a capital letter to start): ")
+
+# planet_index = planets.index(user_planets)
+# print(planet_index + 1)
+
+# print("Here are the planets that are closer than " + user_planets )
+# print(planets[0:planet_index])
+
+# #Display further planets
+
+# print("Here are the planets further than " + user_planets)
+# print(planets[planet_index + 1:])
+
+
+# Working with while loops
+# user_input = ''
+
+# while user_input.lower() != 'done':
+#     user_input = input('Enter a new value, or done when done: ')
+
+# Create the variable for user input
+user_input = ''
+# Create the list to store the values
+inputs = []
+
+# The while loop
+# while user_input.lower() != 'done':
+#     # Check if there's a value in user_input
+#     if user_input:
+#         # Store the value in the list
+#         inputs.append(user_input)
+#         print(inputs)
+#     # Prompt for a new value
+#     user_input = input('Enter a new value, or done when done: ')
+
+
+# new_planet = ''
+# planets = []
+
+# while new_planet.lower() != 'done':
+#     if new_planet:
+#         planets.append(new_planet)
+        
+#     new_planet = input("Enter a new planet, or donw when done: ")
+
+# print(planets)
+
+# Use for loop with lists
+
+# countdown = [4, 3, 2, 1, 0]
+# for number in countdown:
+#     print(number)
+# print("Blast off!! 🚀")
+
+
+# from time import sleep
+
+# countdown = [4, 3, 2, 1, 0]
+
+# for number in countdown:
+#     print(number)
+#     sleep(0)  # Wait 1 second
+# print("🚀🚀🚀🚀")
+
+new_planet = ''
+planets = []
+
+while new_planet.lower() != 'done':
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input('Enter a new planet or done if done: ')
+
 print(planets)
 
-planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
-print(f"There are", len(planets), "planets")
-
-planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
-planets.append('Pluto')
-print("Actually, there are", len(planets), "planets")
-
-print(planets[-1], "is the last planet")
-
-
-
-#Use min() and max() with lists
-
-gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
-bus_weight = 12650 # in Newtons, on Earth
-
-print("On Earth, a double-decker bus weighs", bus_weight, "N")
-print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "N")
-print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "N")
-
-#Slice Lists
-
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-planets_before_earth = planets[0:2]
-print(planets_before_earth)
-
-#To get all the planets after Earth, start at the third and go to the eightplanets_after_earth = 
-
-planets_after_earth = planets[3:8]
-print(planets_after_earth)
-
-#Join Lists
-
-amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
-galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
-
-regular_satellite_moons = amalthea_group + galilean_moons
-print("The regular satellite moons of Jupiter are", regular_satellite_moons)
-
-#Sort Lists
-
-amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
-galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
-
-regular_satellite_moons = amalthea_group + galilean_moons
-regular_satellite_moons.sort()
-print("The regular satellite moons of Jupiter are", regular_satellite_moons)
-
-#Reverse sort
-
-amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
-galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
-
-regular_satellite_moons = amalthea_group + galilean_moons
-regular_satellite_moons.sort(reverse=True)
-print("The regular satellite moons of Jupiter are", regular_satellite_moons)
-
-
-#Exercise
-
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-
-user_planets = input("Please enter the name of the planet (with a capital letter to start): ")
-
-planet_index = planets.index(user_planets)
-print(planet_index + 1)
-
-print("Here are the planets that are closer than " + user_planets )
-print(planets[0:planet_index])
-
-#Display further planets
-
-print("Here are the planets further than " + user_planets)
-print(planets[planet_index + 1:])
-
+for planet in planets:
+    print(planet)
