28fef207536431ddf31a31abb46c01bb6b4abf1d
unknown <davidoluwadolapo@gmail.com>
2023-10-25 03:53:31 +0100

    practicing basic concepts

---
+++ practice.py
@@ -0,0 +1,97 @@
+# first_planet_input = input('Enter the distance from the sun for the first planet in km:')
+# second_planet_input = input('Enter the distance from the sun for the second planet in km:')
+
+
+# first_planet = int(first_planet_input)
+# second_planet = int(second_planet_input)
+
+# distance_km = second_planet - first_planet
+# print(abs(distance_km))
+
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# print("The first planet is", planets[0])
+# print("The second planet is", planets[1])
+# print("The third planet is", planets[2])
+
+# planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# # planets[3] = "Red Planet"
+# print("Mars is also known as", planets[3])
+
+#Determinng the length of a list
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+number_of_planets = len(planets)
+print("There are", number_of_planets, "planets in the solar system.")
+
+#Add values to lists
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets.append("Pluto")
+number_of_planets = len(planets)
+print("There are actually", number_of_planets, "planets in the solar system.")
+
+#Remove value from lists
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+planets.pop()  # Goodbye, Pluto
+number_of_planets = len(planets)
+print("No, there are definitely", number_of_planets, "planets in the solar system.")
+
+#Use negative indexes
+
+print("The first planet is", planets[0])
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print("The last planet is", planets[-1])
+print("The penultimate planet is", planets[-2])
+
+#Find a value in a list
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+jupiter_index = planets.index("Jupiter")
+print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+#Exercise
+
+planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets)
+
+planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(f"There are", len(planets), "planets")
+
+planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+planets.append('Pluto')
+print("Actually, there are", len(planets), "planets")
+
+print(planets[-1], "is the last planet")
+
+
+
+#Use min() and max() with lists
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+bus_weight = 12650 # in Newtons, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "N")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "N")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "N")
+
+#Slice Lists
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+
+#To get all the planets after Earth, start at the third and go to the eightplanets_after_earth = 
+
+planets_after_earth = planets[3:8]
+print(planets_after_earth)
+
+#Join Lists
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+#Sort Lists
+
