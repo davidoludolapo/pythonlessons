0d3d7ef69e92009878c9fc5c6f0f9f8f55c0802f
unknown <davidoluwadolapo@gmail.com>
2023-10-25 04:23:55 +0100

    working with lists

--- practice.py
+++ practice.py
@@ -95,3 +95,37 @@ print("The regular satellite moons of Jupiter are", regular_satellite_moons)
 
 #Sort Lists
 
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+regular_satellite_moons.sort()
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+#Reverse sort
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+regular_satellite_moons.sort(reverse=True)
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+
+#Exercise
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+user_planets = input("Please enter the name of the planet (with a capital letter to start): ")
+
+planet_index = planets.index(user_planets)
+print(planet_index + 1)
+
+print("Here are the planets that are closer than " + user_planets )
+print(planets[0:planet_index])
+
+#Display further planets
+
+print("Here are the planets further than " + user_planets)
+print(planets[planet_index + 1:])
+
