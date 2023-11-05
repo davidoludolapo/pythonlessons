b09f29aedfd540d766b1ef5c1b11b1cc4a0748e5
unknown <davidoluwadolapo@gmail.com>
2023-11-05 23:35:07 +0100

    add while loop

--- LESSONS/conditionals.py
+++ LESSONS/conditionals.py
@@ -1,25 +1,28 @@
 calc_to_units = 24
 name_of_unit = "hours"
 
-def days_to_units(num_of_days):
-    # print(num_of_days  > 0)
-    if num_of_days > 0:
-        return f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}"
-    elif num_of_days == 0:
-        return "you entered zero, not valid. PLease eneter a valid positive number"
 
-    # else:
-    #     return "you entered a neagtive value, so no conversion for you"
+def days_to_units(num_of_days):
+
+    return f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}"
 
 
 def validate_and_execute():
-    if user_input.isdigit():
-    # user_input_number =  int(user_input) # casting - turning a value from one data type to another
-        calc_value = days_to_units(int(user_input))
-        print(calc_value)
-    else:
+ 
+
+    try:
+        user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calc_value = days_to_units(int(user_input))
+            print(calc_value)
+        elif user_input_number == 0:
+            print("you entered zero, not valid. PLease eneter a valid positive number")
+        else:
+            print("You've entered a negative number")
+
+    except ValueError:
         print("Your input is not a valid number")
 
-user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-
-validate_and_execute()
+while True:
+    validate_and_execute()
